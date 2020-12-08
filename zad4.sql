95d997b63ccc18ba9a417fc1fbfc343da238f9b3
Your Name <toni@orqafpv.com>
2020-12-08 08:30:37 +0100

    Bug fix

--- zad4.sql
+++ zad4.sql
@@ -1,11 +1,12 @@
 --Iz tablice student uzimamo ime i prezime, pomocu AVG racunamo prosjek iz tablice ispit, pomocu WHERE definiramo ocjene veće od 1 i grupiramo podatke ime, prezime iz tablice student--
 SELECT ime, prezime, AVG(ocjena) AS 'prosjek'
 FROM ispit, student
-WHERE ispit.ocjena > 1
+WHERE ocjena > 1
 GROUP BY ime, prezime;
 
 --Uzimamo maticni broj studenta iz tablice student, prosjek ocijena ali filtriramo pomocu naredbe WHERE.
 SELECT mbrStud, AVG(ocjena) AS 'prosjek'
 FROM ispit
-WHERE ocjena > 2.5
-GROUP BY mbrStud;
+WHERE ocjena > 1
+GROUP BY mbrStud
+HAVING AVG(ocjena) > 2.5;
